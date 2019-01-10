16bac6c2abab80e962585855500406d21b28951a
user <user@DESKTOP-6R9K122>
2019-01-11 01:00:02 +0900

    homework

--- src/icehs/science/Chapter04/SalaryTest.java
+++ src/icehs/science/Chapter04/SalaryTest.java
@@ -7,11 +7,11 @@ public class SalaryTest {
 		
 		int pay = 1000000;
 		int yearpay = pay * 12;
-		int taxyearpay = yearpay * (9 / 10);
+		int taxyearpay = yearpay * 9 / 10;
 		System.out.println("연봉 : " + yearpay + ", 세후 연봉 : " + taxyearpay);
 		
-		int bonus = pay * (1 / 5) * 3;
-		int taxbonus = bonus * (189 / 200);
+		int bonus = pay * 1 / 5 * 4;
+		int taxbonus = bonus * 189 / 200;
 		System.out.println("보너스 : " + bonus + ", 세후 보너스 : " + taxbonus);
 		
 		System.out.println("지급액 : " + (taxyearpay + taxbonus));
