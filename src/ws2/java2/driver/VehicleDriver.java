567f1249ead93327fe8b1bc576cabc2c3517fca4
admin <admin@10.137.138.104>
2019-01-15 17:43:36 +0900

    homework

---
+++ src/ws2/java2/driver/VehicleDriver.java
@@ -0,0 +1,10 @@
+package ws2.java2.driver;
+
+import ws2.java2.controller.*;
+
+public class VehicleDriver {
+	public static void main(String[] args) {
+		VehicleManager vm = new VehicleManager();
+		vm.displayVehicles(">> 재고 목록 <<");
+	}
+}
